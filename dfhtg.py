87a882fe3acc4ec6d3263896097c2a73b3d0cbfa
izabelle02 <al.maria.silva@impatech.edu.br>
2025-08-11 16:23:58 -0300

    imprime

---
+++ dfhtg.py
@@ -0,0 +1,11 @@
+import mytools
+
+num1 = int(input('insira o num de casas decimais do pi: '))
+print('pi com %d casas decimais: ' %num1)
+mytools.pi_real(num1)
+print()
+
+num2 = int(input('insira o num de casas decimais do e: '))
+print('e com %d casas decimais: ' %num2)
+mytools.e_real(num2)
+print()
